eb0aa2beacfef2142a8c1c5c0249a72ccd5c7f91
iven_k <koethke@th-brandenburg.de>
2019-06-15 21:39:34 +0200

    Added new view to represent todos in list

---
+++ app/src/main/java/com/toapp/CustomScrollElement.java
@@ -0,0 +1,23 @@
+package com.toapp.ui.login;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomScrollElement extends View {
+    private String name;
+    private String dueDate;
+    private String done;
+    private String important;
+
+
+    public CustomScrollElement(Context context) {
+        super(context);
+    }
+
+    public CustomScrollElement(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+}
