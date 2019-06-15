a5117c9774f25758bb99bae2d0a36ce5b647cee3
iven_k <koethke@th-brandenburg.de>
2019-06-15 22:04:26 +0200

    Implemented method to match priority and color.

--- app/src/main/java/com/toapp/CustomScrollElement.java
+++ app/src/main/java/com/toapp/CustomScrollElement.java
@@ -10,19 +10,18 @@ import android.widget.TextView;
 public class CustomScrollElement extends LinearLayout {
     private final String TAG = "CustomScrollElement";
 
-    // not really usefull
+    // not really useful
     protected CustomScrollElement(Context context) {
         super(context);
         inflate(context, R.layout.custom_scroll_element, this);
     }
 
-    public CustomScrollElement(Context context, String name, String dueDate, int priority) {
+    public CustomScrollElement(Context context, String name, String dueDate, EPriority priority) {
         super(context);
         inflate(context, R.layout.custom_scroll_element, this);
         ((TextView)findViewById(R.id.customScrollTodoName)).setText(name);
         ((TextView)findViewById(R.id.customScrollTodoName)).setText(dueDate);
-
-
+        findViewById(R.id.customScrollTodoPriority).setBackgroundColor(matchPriorityColor(priority));
     }
 
     // we do not want that to be called
@@ -31,5 +30,18 @@ public class CustomScrollElement extends LinearLayout {
         inflate(context, R.layout.custom_scroll_element, this);
     }
 
-
+    private int matchPriorityColor(EPriority priority) {
+        switch (priority) {
+            case LOW:
+                return R.color.scrollElementPriority1;
+            case MEDIUM:
+                return R.color.scrollElementPriority2;
+            case HIGH:
+                return R.color.scrollElementPriority3;
+            default:
+                //this should never happen !
+                Log.e(TAG, "matchPriorityColor: Priority out of bounds. Could not be matched !");
+                return R.color.scrollElementPriority1; // TODO: maybe choose different color for this in order to make debugging easier.
+            }
+    }
 }
