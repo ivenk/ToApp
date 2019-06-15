c210b0629722e51dbf8d8a656a7a9d1d71e81c12
iven_k <koethke@th-brandenburg.de>
2019-06-15 23:29:02 +0200

    Implemented date and time picker partly and changed the db layout accordingly. not finished

--- app/src/main/java/com/toapp/AppDatabase.java
+++ app/src/main/java/com/toapp/AppDatabase.java
@@ -6,14 +6,14 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities=Todo.class,exportSchema = false, version = 2)
+@Database(entities=Todo.class, exportSchema = false, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String DB_NAME = "app_db";
     private static AppDatabase instance;
 
     public static synchronized AppDatabase getInstance(Context context) {
         if(instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DB_NAME).allowMainThreadQueries().build();
+            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DB_NAME).allowMainThreadQueries().fallbackToDestructiveMigration().build();
         }
         return instance;
     }
--- app/src/main/java/com/toapp/NewTodoActivity.java
+++ app/src/main/java/com/toapp/NewTodoActivity.java
@@ -7,11 +7,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 
 public class NewTodoActivity extends AppCompatActivity {
     private final String TAG = "NewTodoActivity";
@@ -29,13 +36,29 @@ public class NewTodoActivity extends AppCompatActivity {
         setTitle("New Todo");
     }
 
-    public void onCreateButton(View view) {
-        Log.i(TAG, "onCreateButton: Called! No action implemented yet");
+    public void onDateClicked(View view) {
+        Log.i(TAG, "onDateClicked: ");
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
 
+    public void onTimeClicked(View view) {
+        Log.i(TAG, "onTimeClicked: ");
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void onCreateButton(View view) {
         //TODO get more infos from page
         TextView titleView = findViewById(R.id.title_input_label);
         TextView descriptionView = findViewById(R.id.description_input_label);
         Switch favouriteView = findViewById(R.id.favourite_switch);
+
+        TextView date = findViewById(R.id.input_date);
+        TextView time = findViewById(R.id.input_time);
+        Log.i(TAG, "onCreateButton: date: " + date);
+        Log.i(TAG, "onCreateButton: time: " + time);
+
 
         Todo t = new Todo(titleView.getText().toString(), descriptionView.getText().toString(), false, favouriteView.isChecked());
         AppDatabase.getInstance(getApplicationContext()).todoDao().insert(t);
