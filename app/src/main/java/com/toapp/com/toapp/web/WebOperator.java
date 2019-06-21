2d4aa3cd06889e5811d9e94c6fc35471d1449934
iven_k <koethke@th-brandenburg.de>
2019-06-22 00:58:28 +0200

    Finished functionality for getAllTodos. Now builds todos.

--- app/src/main/java/com/toapp/com/toapp/web/WebOperator.java
+++ app/src/main/java/com/toapp/com/toapp/web/WebOperator.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WebOperator implements WebAPI {
@@ -27,8 +28,19 @@ public class WebOperator implements WebAPI {
             List<JSONObject> rawTodos = webConnector.readAllTodos();
             if(rawTodos == null) //something went wrong earlier, nothing to do here. Error was thrown already.
                 return null;
-            int id = rawTodos.get(0).getInt("id");
-            Log.i(TAG, "readAllTodos: Found id was : " + id);
+
+            // build todos
+            List<Todo> result = new ArrayList<>();
+            for (JSONObject jsobject: rawTodos) {
+                int id = jsobject.getInt("id");
+                String name = jsobject.getString("name");
+                String description = jsobject.getString("description");
+                Long dueDate = jsobject.getLong("expiry");
+                Boolean done = jsobject.getBoolean("done");
+                Boolean favourite = jsobject.getBoolean("favourite");
+                result.add(new Todo(id, name, description, done, favourite, dueDate));
+            }
+            return result;
         } catch(JSONException jse) {
             Log.e(TAG, "readAllTodos: JSONException while trying to fetch results from the webserver.", jse);
         }
