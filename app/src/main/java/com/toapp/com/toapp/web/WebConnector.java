9a87b176abd162e0ba9d330d72fde0061f2940ce
iven_k <koethke@th-brandenburg.de>
2019-06-21 02:58:24 +0200

    Web connector now succesfully connects to local server and receives json.

--- app/src/main/java/com/toapp/com/toapp/web/WebConnector.java
+++ app/src/main/java/com/toapp/com/toapp/web/WebConnector.java
@@ -6,59 +6,65 @@ import androidx.annotation.Nullable;
 
 import com.toapp.Todo;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WebConnector implements WebAPI {
+public class WebConnector{
     private final String TAG = "WebConnector";
 
-    private final String BASEURI = "http://localhost:8080/api/";
+    private final String BASEURI = "http://10.0.2.2:8080/api/";
     private final String TODOENPOINT = "todos";
     private final String USERENTPOINT = "users";
 
-
-
-    @Override
     public boolean createTodos(List<Todo> todos) {
         return false;
     }
 
-    @Override
-    public List<Todo> readAllTodos() throws MalformedURLException, IOException{
-        List<Todo> results = new ArrayList<>();
-        URL url = new URL(BASEURI + TODOENPOINT);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    public List<JSONObject> readAllTodos() throws IOException{
+        HttpURLConnection urlConnection = null;
         try {
+            URL url = new URL(BASEURI + TODOENPOINT);
+            Log.i(TAG, "readAllTodos: connection url is " + url.toString());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+          //  urlConnection.connect();
+
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
+            if(in == null) {
+                Log.e(TAG, "readAllTodos: fail !!!");
+            }
+            return UnmarshallHelper.unmarshall(in);
             //TODO read and unmarshall stream
         } catch (MalformedURLException mue) {
-            Log.e(TAG, "readAllTodos: ", mue);
+            Log.e(TAG, "readAllTodos: Malformed url !", mue);
+            throw mue;
         } catch (IOException ioe){
-            Log.e(TAG, "readAllTodos: ", ioe);
+            Log.e(TAG, "readAllTodos: IOException ocured while trying to unmarshall input stream.", ioe);
+            throw ioe;
         }finally {
-            urlConnection.disconnect();
+            if(urlConnection != null)
+                urlConnection.disconnect();
         }
-        return results;
     }
 
-    @Override
     public Todo readTodo(int id) {
         return null;
     }
 
-    @Override
     public boolean updateTodo(int id, Todo newVersion) {
         return false;
     }
 
-    @Override
     public boolean deleteTodo(int id) {
         return false;
     }
