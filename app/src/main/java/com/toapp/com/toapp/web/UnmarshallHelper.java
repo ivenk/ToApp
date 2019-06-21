b1f76c77ab49c834a4566665d343e063920e2f8f
iven_k <koethke@th-brandenburg.de>
2019-06-21 02:56:26 +0200

    Implemented unmarshallhelper. Currently very simple ! Just reads all lines and then cast that to a list of jsonobjects.

--- app/src/main/java/com/toapp/com/toapp/web/UnmarshallHelper.java
+++ app/src/main/java/com/toapp/com/toapp/web/UnmarshallHelper.java
@@ -1,5 +1,44 @@
 package com.toapp.com.toapp.web;
 
-public class UnmarshallHelper {
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnmarshallHelper {
+    private static final String TAG = "UnmarshallHelper";
+
+    public static List<JSONObject> unmarshall(InputStream is) {
+        List<JSONObject> results = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder strBuilder = new StringBuilder();
+        try {
+            String line;
+            while((line = br.readLine()) != null) {
+                strBuilder.append(line);
+            }
+            JSONArray jsonArray = new JSONArray(strBuilder.toString());
+
+            for (int i =0; i< jsonArray.length(); i++) {
+                results.add(jsonArray.getJSONObject(i));
+            }
+            return results;
+        }
+        catch (IOException ioe) {
+            Log.e(TAG, "unmarshall: ", ioe);
+            return null;
+        } catch (JSONException jse) {
+            Log.e(TAG, "unmarshall: ", jse);
+            return null;
+        }
+    }
 }
