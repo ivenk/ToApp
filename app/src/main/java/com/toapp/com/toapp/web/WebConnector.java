2bf1c0538a63b3767428bf09bc650e91967d2c86
iven_k <koethke@th-brandenburg.de>
2019-06-21 16:20:32 +0200

    Adapted exception handling to best practices and started implementing deleteAlltodos.

--- app/src/main/java/com/toapp/com/toapp/web/WebConnector.java
+++ app/src/main/java/com/toapp/com/toapp/web/WebConnector.java
@@ -6,6 +6,8 @@ import androidx.annotation.Nullable;
 
 import com.toapp.Todo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -13,8 +15,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,43 +31,69 @@ public class WebConnector{
         return false;
     }
 
-    public List<JSONObject> readAllTodos() throws IOException{
+    @Nullable
+    public List<JSONObject> readAllTodos(){
         HttpURLConnection urlConnection = null;
         try {
             URL url = new URL(BASEURI + TODOENPOINT);
             Log.i(TAG, "readAllTodos: connection url is " + url.toString());
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
-          //  urlConnection.connect();
 
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            JSONArray jsonArray = UnmarshallHelper.unmarshall(in);
 
-            if(in == null) {
-                Log.e(TAG, "readAllTodos: fail !!!");
+            List<JSONObject> results = new ArrayList<>();
+
+            for (int i =0; i< jsonArray.length(); i++) {
+                results.add(jsonArray.getJSONObject(i));
             }
-            return UnmarshallHelper.unmarshall(in);
+            return results;
             //TODO read and unmarshall stream
+        } catch (JSONException jse){
+            Log.e(TAG, "readAllTodos: JSONException occured while trying to parse fetched results to jsonobjects.", jse);
         } catch (MalformedURLException mue) {
             Log.e(TAG, "readAllTodos: Malformed url !", mue);
-            throw mue;
         } catch (IOException ioe){
             Log.e(TAG, "readAllTodos: IOException ocured while trying to unmarshall input stream.", ioe);
-            throw ioe;
         }finally {
             if(urlConnection != null)
                 urlConnection.disconnect();
         }
+        return null;
     }
 
     public Todo readTodo(int id) {
+        Log.e(TAG, "deleteTodo: Not yet implemented !");
         return null;
     }
 
     public boolean updateTodo(int id, Todo newVersion) {
+        Log.e(TAG, "deleteTodo: Not yet implemented !");
         return false;
     }
 
     public boolean deleteTodo(int id) {
+        Log.e(TAG, "deleteTodo: Not yet implemented !");
+        return false;
+    }
+    
+    public boolean deleteAllTodos() {
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(BASEURI + TODOENPOINT);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            JSONArray jsonArray = UnmarshallHelper.unmarshall(in);
+
+        } catch (MalformedURLException mue) {
+            Log.e(TAG, "deleteAllTodos: ", mue);
+        } catch (ProtocolException pte) {
+            Log.e(TAG, "deleteAllTodos: ", pte);
+        } catch (IOException ioe){
+            Log.e(TAG, "deleteAllTodos: ", ioe);
+        }
         return false;
     }
 }
