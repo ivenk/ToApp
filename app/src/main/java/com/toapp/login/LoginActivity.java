c9fed843281df65b876d291f6a4c73ee570164d8
iven_k <koethke@th-brandenburg.de>
2019-06-22 04:15:51 +0200

    Formating.

--- app/src/main/java/com/toapp/login/LoginActivity.java
+++ app/src/main/java/com/toapp/login/LoginActivity.java
@@ -20,7 +20,5 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
-
 }
