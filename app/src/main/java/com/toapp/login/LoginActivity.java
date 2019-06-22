b83654cdf93919d191b72cc0084bc630717ead84
iven_k <koethke@th-brandenburg.de>
2019-06-22 03:24:58 +0200

    Created own login activity. Reused xml file from android login.

---
+++ app/src/main/java/com/toapp/login/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.toapp.login;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.toapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
