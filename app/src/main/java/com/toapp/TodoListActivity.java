85d97337282e5a1a0e7e9a2c9d46857e03901df2
iven_k <koethke@th-brandenburg.de>
2019-06-22 02:43:23 +0200

    Removed error checking.

--- app/src/main/java/com/toapp/TodoListActivity.java
+++ app/src/main/java/com/toapp/TodoListActivity.java
@@ -40,13 +40,8 @@ public class TodoListActivity extends AppCompatActivity {
             public void run() {
                 super.run();
                 WebOperator webOperator = new WebOperator();
-                try {
-                    webOperator.readAllTodos();
-                } catch (IOException e) {
-                    Log.e(TAG, "onCreate: Could not reach webserver !", e);
-                }catch (JSONException jse) {
-                    Log.e(TAG, "run: Could not parse received JSON !", jse);
-                }
+                //webOperator.readAllTodos();
+                webOperator.deleteAllTodos();
             }
         }.start();
     }
