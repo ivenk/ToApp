8c6a06f32b32d2016abe3acce73b6551fd7e85cf
iven_k <koethke@th-brandenburg.de>
2019-06-25 22:58:49 +0200

    Now takes the done state of the todo aswell and sets the chckbox accordingly.

--- app/src/main/java/com/toapp/CustomScrollElement.java
+++ app/src/main/java/com/toapp/CustomScrollElement.java
@@ -4,19 +4,21 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class CustomScrollElement extends LinearLayout {
     private final String TAG = "CustomScrollElement";
 
-    public CustomScrollElement(Context context, String name, String dueDate, boolean isFavourite, int id) {
+    public CustomScrollElement(Context context, String name, String dueDate, boolean isFavourite, boolean isDone, int id) {
         super(context);
         View view = inflate(context, R.layout.custom_scroll_element, this);
 
         ((TextView)view.findViewById(R.id.customScrollTodoId)).setText(Integer.toString(id));
         ((TextView)findViewById(R.id.customScrollTodoName)).setText(name);
         ((TextView)findViewById(R.id.customScrollTodoDate)).setText(dueDate);
+        ((CheckBox)findViewById(R.id.customScrollCheckBox)).setChecked(isDone);
 
         int color = getResources().getColor(R.color.scrollElementFavouriteNo);
         if (isFavourite)
