6c880862acc1f4e10e1ac8d335f351c3bad9e070
iven_k <koethke@th-brandenburg.de>
2019-06-26 22:28:28 +0200

    Created contact class to represent android contacts.

---
+++ app/src/main/java/com/toapp/data/Contact.java
@@ -0,0 +1,54 @@
+package com.toapp.data;
+
+/**
+ * Represents a contact.
+ */
+public class Contact {
+    private int id;
+    private String name;
+    private String number;
+    private String email;
+
+    public Contact() {
+
+    }
+
+    public Contact(int id, String name, String number, String email) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
