741b11356f85e87b8b50c57b727c37c665807a7d
iven_k <koethke@th-brandenburg.de>
2019-06-27 02:57:26 +0200

    Added layout for contact scroller.

--- app/src/main/java/com/toapp/ContactScroller.java
+++ app/src/main/java/com/toapp/ContactScroller.java
@@ -107,6 +107,10 @@ public class ContactScroller extends Fragment implements View.OnClickListener {
         }
     }
 
+    public void onContactDelete(View view) {
+
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
