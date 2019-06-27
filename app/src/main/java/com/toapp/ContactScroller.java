9a64701b9f9465117f97fc8eaa655900aa4cf49e
iven_k <koethke@th-brandenburg.de>
2019-06-27 16:45:12 +0200

    Added possibly unused initialisation logic.

--- app/src/main/java/com/toapp/ContactScroller.java
+++ app/src/main/java/com/toapp/ContactScroller.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -18,6 +19,9 @@ import android.widget.TextView;
 
 import com.toapp.data.Contact;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,15 +35,9 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class ContactScroller extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String CONTACTS_PARAM = "contacts";
     public static final String TAG = "ContactScroller";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -57,16 +55,18 @@ public class ContactScroller extends Fragment implements View.OnClickListener {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ContactScroller.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ContactScroller newInstance(String param1, String param2) {
+    public static ContactScroller newInstance(@Nullable List<Contact> contacts) {
         ContactScroller fragment = new ContactScroller();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        if(contacts != null) {
+            String[] strings = new String[contacts.size()];
+            for (int i = 0; i< contacts.size(); i++) {
+                strings[i] = contacts.get(i).toJSON().toString();
+            }
+            args.putStringArray(CONTACTS_PARAM, strings);
+        }
         fragment.setArguments(args);
         return fragment;
     }
@@ -75,12 +75,20 @@ public class ContactScroller extends Fragment implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String[] contactsJson = getArguments().getStringArray(CONTACTS_PARAM);
+            if (contactsJson != null) {
+                List<Contact> building = new ArrayList<>();
+                try {
+                    for(String str : contactsJson) {
+                        building.add(new Contact(new JSONObject(str)));
+                    }
+                }catch (JSONException jse) {
+                    Log.e(TAG, "onCreate: JSONError occurred while trying to build contacts passed to ContactScroller ", jse);
+                }
+                contacts = building;
+            }
         }
-
         contacts = new ArrayList<>();
-        //TODO add passed contacts
     }
 
     @Override
